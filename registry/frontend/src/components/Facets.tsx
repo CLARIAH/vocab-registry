ffc53e1f9ca0866f8f7b0a143a5aae04c08ca271
kerim1 <kerim.meijer@di.huc.knaw.nl>
2025-05-28 16:02:20 +0200

    Change the flex settings for the topic facets

--- registry/frontend/src/components/Facets.tsx
+++ registry/frontend/src/components/Facets.tsx
@@ -3,18 +3,17 @@ import {FreeTextFacet, ListFacet} from '@knaw-huc/browser-base-react';
 export default function Facets() {
     const defaultFacetProps = {
         url: '/facet',
-        flex: false,
         usePost: true,
         isHidden: false
     };
 
     return <>
         <FreeTextFacet/>
-        <ListFacet name="Syntax" field="syntax" {...defaultFacetProps}/>
-        <ListFacet name="Type" field="kos" {...defaultFacetProps}/>
-        <ListFacet name="Entity" field="entity" {...defaultFacetProps}/>
-        <ListFacet name="Topic (NWO)" field="nwo" {...defaultFacetProps}/>
-        <ListFacet name="Topic (UNESCO)" field="unesco" {...defaultFacetProps}/>
-        <ListFacet name="Registry" field="registries" {...defaultFacetProps}/>
+        <ListFacet name="Syntax" field="syntax" {...defaultFacetProps} flex={false}/>
+        <ListFacet name="Type" field="kos" {...defaultFacetProps} flex={false}/>
+        <ListFacet name="Entity" field="entity" {...defaultFacetProps} flex={false}/>
+        <ListFacet name="Topic (NWO)" field="nwo" {...defaultFacetProps} flex={true}/>
+        <ListFacet name="Topic (UNESCO)" field="unesco" {...defaultFacetProps} flex={true}/>
+        <ListFacet name="Registry" field="registries" {...defaultFacetProps} flex={false}/>
     </>;
 }
